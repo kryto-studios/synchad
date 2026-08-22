c69a8ef1257dfc2f884d6adc5f4db7a208eb4f21
srijan-institute <studios.kryto@gmail.com>
2026-08-22 23:01:11 +0530

    feat: add llms.txt context file and AI crawler permissions for Google AI Overviews

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,29 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/api/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/admin",
+      },
+      {
+        userAgent: "Google-Extended",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        allow: "/",
+      },
+    ],
     sitemap: "https://synchad.com/sitemap.xml",
   };
 }
